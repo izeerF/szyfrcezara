e175d64a638bc2beb84cb5858ae98bf8c3d1e891
izeerF <101408731+izeerF@users.noreply.github.com>
2022-03-18 15:05:07 +0100

    Dodaj pliki projektów.

---
+++ szyfrcezara/Program.cs
@@ -0,0 +1,19 @@
+﻿string alfabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻ";
+Console.WriteLine("Podaj wzór kodu do szyfrowania:");
+
+int szyfr;
+Int32.TryParse(Console.ReadLine(), out szyfr);
+
+
+
+Console.WriteLine("Podaj Tekst do zaszyfrowania");
+string tekst = Console.ReadLine().Replace(" ", "");
+tekst = tekst.ToUpper();
+string wyjscie = "";
+for (int i = 0; i < tekst.Length; i++)
+{
+    alfabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻ";
+    var poszyfr = (alfabet.IndexOf(tekst[i])+szyfr)%alfabet.Length;
+    wyjscie += alfabet[poszyfr].ToString();
+}
+Console.WriteLine(wyjscie);
